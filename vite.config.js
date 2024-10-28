30e7dd8b3c4825eeeb46fbffd78ac35b0a0910e3
Mutiulrasool <mutiulrasool769@gmail.com>
2024-10-29 01:24:35 +0400

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Assignment-1',
+  base: '/React-Assignment-1/',
 })
